5ba31843e0c8adf286f78a7422f2e50d4ad682e6
Brandon Babcock <brandon.babcock@atlaswerks.com>
2024-07-18 13:57:35 -0500

    testing script install issues

--- Scripts/installPackages.ps1
+++ Scripts/installPackages.ps1
@@ -1,16 +1,17 @@
+#This leverages the run command on the VM itself. 90 minute timeout.
+
+
 #Install Steam
 # Define the URL for the Steam installer
 $steamInstallerUrl = "https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe"
-# Define the path where the installer will be downloaded
-$installerPath = "$env:USERPROFILE\Downloads\SteamSetup.exe"
 # Download the Steam installer
-Invoke-WebRequest -Uri $steamInstallerUrl -OutFile $installerPath
+Invoke-WebRequest -Uri $steamInstallerUrl -OutFile 'SteamSetup.exe'
 # Run the installer silently
-Start-Process -FilePath $installerPath -ArgumentList "/S" -NoNewWindow -Wait
+Start-Process -FilePath 'SteamSetup.exe' -ArgumentList "/S" -NoNewWindow -Wait
 # Remove the installer after installation
-Remove-Item -Path $installerPath
+Remove-Item -Path 'SteamSetup.exe'
 
-
+<# 
 #Install Sunshine
 # Define the URL for the Sunshine installer
 $sunshineInstallerUrl = "https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-windows-installer.exe"
@@ -36,3 +37,16 @@ Start-Process -FilePath $installerPath -ArgumentList "/S" -NoNewWindow -Wait
 # Remove the installer after installation
 Remove-Item -Path $installerPath
 
+
+#Install TigerVNC Server
+# Define the URL for the TigerVNC
+$tigerInstallerUrl = "https://phoenixnap.dl.sourceforge.net/project/tigervnc/stable/1.13.1/tigervnc64-winvnc-1.13.1.exe?viasf=1"
+# Define the path where the installer will be downloaded
+$installerPath = "$env:USERPROFILE\Downloads\ParsecVDisplay-v0.45-setup.exe"
+# Download the Sunshine installer
+Invoke-WebRequest -Uri $tigerInstallerUrl -OutFile $installerPath
+# Run the installer silently
+Start-Process -FilePath $installerPath -ArgumentList "/S" -NoNewWindow -Wait
+# Remove the installer after installation
+Remove-Item -Path $installerPath
+ #>

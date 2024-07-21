ed62b2ef321c55fb27c7cf69b3c4b1885ab7332b
Brandon Babcock <brandon.babcock@atlaswerks.com>
2024-07-21 15:53:08 -0500

    reworked script

--- Scripts/installPackages.ps1
+++ Scripts/installPackages.ps1
@@ -7,7 +7,7 @@ $urls = @(
 "https://github.com/LizardByte/Sunshine/releases/latest/download/sunshine-windows-installer.exe",
 "https://github.com/nomi-san/parsec-vdd/releases/download/v0.45.1/ParsecVDisplay-v0.45-setup.exe",
 "https://raw.githubusercontent.com/bbabcock1990/cloud_gaming_vm/main/Scripts/config.zip",
-"https://go.microsoft.com/fwlink/?linkid=2248541"
+"https://download.microsoft.com/download/9/1/e/91ed0d01-1a2c-46ad-b014-51ece3b1936c/amd-software-cloud-edition-23.q3-azure-ngads-v620.exe"
 )
 
 # Define the download directory
@@ -22,29 +22,21 @@ if (-not (Test-Path -Path $downloadDir)) {
 foreach ($url in $urls) {
     # Extract the file name from the URL
     $fileName = [System.IO.Path]::GetFileName($url)
-    
     # Define the full path to the downloaded file
     $filePath = Join-Path -Path $downloadDir -ChildPath $fileName
-    
     # Download the file
     Invoke-WebRequest -Uri $url -OutFile $filePath
+    # Install the file
+    if ($fileName -ne "config.zip") {
+        Start-Process -FilePath $filePath -ArgumentList "/silent"
+    }
 }
 
-<# 
-# Define variables
-$sunshineConfigZip = "https://raw.githubusercontent.com/bbabcock1990/cloud_gaming_vm/main/Scripts/config.zip"  # URL of the ZIP file
-
-# Download the ZIP file
-Invoke-WebRequest -Uri $sunshineConfigZip -OutFile 'config.zip'
-
-# Extract the ZIP file
+# Extract the Sunshine config ZIP file
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory('config.zip', 'C:\Temp') #Need to fix.
+[System.IO.Compression.ZipFile]::ExtractToDirectory('C:\Temp\config.zip', 'C:\Temp') #Need to fix.
 
 # Copy the extracted files to the destination folder, overwriting existing files
 Copy-Item -Path "C:\Temp\config\*" -Destination 'C:\Program Files\Sunshine\config\' -Recurse -Force
 
-# Clean up
-Remove-Item -Path 'config.zip'
-Remove-Item -Path 'C:\Temp' -Recurse
- #>
+Restart-Computer -Force

ed096c72da96488d1c88e37206f2e5afb3f3b658
Brandon Babcock <brandon.babcock@atlaswerks.com>
2024-07-21 17:54:50 -0500

    reworked script

--- Scripts/installPackages.ps1
+++ Scripts/installPackages.ps1
@@ -42,3 +42,5 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 
 # Copy the extracted files to the destination folder, overwriting existing files
 Copy-Item -Path "C:\Temp\config\*" -Destination 'C:\Program Files\Sunshine\config\' -Recurse -Force
+
+Restart-Computer -Force
